e0839de4f6c216b3fe5f3dd38597fc086061f24f
isfttr <lucas.fernandes.df@gmail.com>
2024-04-05 18:06:05 +0000

    edit: alteracao para comportar possibilidades de falta de comentario

--- xmlextractor.py
+++ xmlextractor.py
@@ -1,8 +1,9 @@
 import xml.etree.ElementTree as ET
+import pandas as pd
 
 tree = ET.parse('P2777.xml')
 
-def extract_data(despacho):
+def extract_data(despacho: any) -> list:
     data = []
 
     codigo_despacho = despacho.find('codigo').text
@@ -11,22 +12,23 @@ def extract_data(despacho):
     numero_processo = processo.find('numero').text if processo is not None and processo.find('numero') is not None else None
     data_deposito = processo.find('data-deposito').text if processo is not None and processo.find('data-deposito') is not None else None
     titulares = processo.findall('.//titular') if processo is not None else []
-    comentario = despacho.find('comentario').text if despacho is not None and despacho.find('comentario') is not None else None
     
+    comentario = despacho.find('comentario').text if despacho.find('comentario') is not None else None
+    if comentario is not None:
+        data.append([codigo_despacho, titulo, numero_processo, data_deposito, comentario])
+    else:
+        data.append([codigo_despacho, titulo, numero_processo, data_deposito, None])
+   
     for titular in titulares:
         nome_completo = titular.find('nome-completo').text
         endereco = titular.find('endereco')
         uf = endereco.find('uf').text if endereco is not None and endereco.find('uf') is not None else None
         pais = endereco.find('pais/sigla').text if endereco is not None and endereco.find('pais/sigla') is not None else None
-        data.append((codigo_despacho, titulo, numero_processo, data_deposito, nome_completo, uf, pais))
-    
-    data.append(comentario)
-    
+        data[-1].extend([nome_completo, uf, pais])
+
     return data
 
 data = []
 root = tree.getroot()
 for despacho in root.findall('despacho'):
     data.extend(extract_data(despacho))
-
-print(data)
