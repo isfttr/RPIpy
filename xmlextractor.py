b072995d7ced799af247a2f6d4f707e02c791afc
isfttr <lucas.fernandes.df@gmail.com>
2024-04-06 16:59:05 +0000

    add: flatten_data function

--- xmlextractor.py
+++ xmlextractor.py
@@ -3,11 +3,15 @@ import pandas as pd
 import uuid 
 from uuid import uuid4
 
+
+# Prompt inicial
 numero_rpi_str = input("Número de RPI: ")
 numero_rpi = int(numero_rpi_str)
 
+# Parsing do arquivo XML
 tree = ET.parse('P'+str(numero_rpi)+'.xml')
 
+# Função de extração de todos os despachos contidos no XML
 def extract_data(despacho: any) -> list:
     data = []
 
@@ -40,27 +44,36 @@ root = tree.getroot()
 for despacho in root.findall('despacho'):
     data.extend(extract_data(despacho))
 
-#df = pd.DataFrame(data, columns=['despacho_id','codigo_despacho', 'titulo', 'numero_processo', 'data_deposito', 'comentario', 'titular'])
+# Essa função organiza os dados para serem compatíveis com dataframe
+def flatten_data(data: list) -> any:
 
-flattened_data = []
-for row in data:
-    despacho_id = row[0]
-    codigo_despacho = row[1]
-    titulo = row[2]
-    numero_processo = row[3]
-    data_deposito = row[4]
-    comentario = row[5]
+    flattened_data = []
     
+    for row in data:
+        despacho_id = row[0]
+        codigo_despacho = row[1]
+        titulo = row[2]
+        numero_processo = row[3]
+        data_deposito = row[4]
+        comentario = row[5]
+        
     for titular_info in row[6:]:
         sequencia_titular = titular_info[0]
         nome_completo = titular_info[1]
         uf = titular_info[2]
         pais = titular_info[3]
         
-        flattened_data.append([despacho_id, codigo_despacho, titulo, numero_processo, data_deposito, comentario, sequencia_titular, nome_completo, uf, pais])
+    flattened_data.append([despacho_id, codigo_despacho, titulo, numero_processo, data_deposito, comentario, sequencia_titular, nome_completo, uf, pais])
 
-# Create DataFrame
-df = pd.DataFrame(flattened_data, columns=['despacho_id', 'codigo_despacho', 'titulo', 'numero_processo', 'data_deposito', 'comentario', 'sequencia_titular', 'nome_completo', 'uf', 'pais'])
-df.to_csv(str('P'+numero_rpi_str), sep=',', index=False, encoding='utf-8')
-print(df)
-#print(data)
+    # Create DataFrame
+    df = pd.DataFrame(flattened_data, columns=['despacho_id', 'codigo_despacho', 'titulo', 'numero_processo', 'data_deposito', 'comentario', 'sequencia_titular', 'nome_completo', 'uf', 'pais'])
+    
+    # Save DataFrame to CSV
+    df.to_csv(str('P'+numero_rpi_str)+'.csv', sep=',', index=False, encoding='utf-8')
+    
+    return df
+
+if __name__ == '__main__':
+
+    extract_data()
+    flatten_data()
