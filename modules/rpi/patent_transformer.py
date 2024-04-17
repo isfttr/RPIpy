34eaefb5f152a5ce084a012ab184ebda680285b8
Lucas F Aguiar <lucas.fernandes.df@gmail.com>
2024-04-17 17:30:48 +0000

    removed: check_xml function, not used

--- modules/rpi/patent_transformer.py
+++ modules/rpi/patent_transformer.py
@@ -6,32 +6,14 @@ from uuid import uuid4
 # sys.path.append('utils/')
 # from utils.cleaner import swipe_csv
 
-def check_xml_exists(missing_rpi_csv: list) -> list:
-    missing_rpi = []
-
-    for numero_rpi in missing_rpi_csv:  
-        xml_file_name = 'P{}.xml'.format(numero_rpi)
-
-        if not os.path.exists(xml_file_name):
-            print(f'CheckXML: {xml_file_name} não existe...')
-            missing_rpi.append(numero_rpi)
-
-    if missing_rpi:
-        print(f'CheckXML: Não foram encontrados os arquivos referentes as RPIs: {", ".join(map(str, missing_rpi))}...')
-    else:
-        print(f'CheckXML: Todos arquivos encontrados!')
-    
-    return missing_rpi or None
-
-
 def check_csv_exists(numero_rpi_start: int, numero_rpi_end: int) -> list:
     missing_rpi_csv = []
 
     for numero_rpi in range(numero_rpi_start, numero_rpi_end+1):  
-        xml_file_name = 'P{}.csv'.format(numero_rpi)
+        csv_file_name = 'P{}.csv'.format(numero_rpi)
 
-        if not os.path.exists(xml_file_name):
-            print(f'CheckCSV: {xml_file_name} não existe...')
+        if not os.path.exists(csv_file_name):
+            print(f'CheckCSV: {csv_file_name} não existe...')
             missing_rpi_csv.append(numero_rpi)
 
     if missing_rpi_csv:
@@ -55,7 +37,6 @@ if __name__ == '__main__':
     numero_rpi_start = int(input('PatentTransformer: Escreva o número de RPI inicial: '))
     numero_rpi_end = int(input('PatentTransformer: Escreva o número de RPI final: '))
     missing_rpi_csv = check_csv_exists(numero_rpi_start, numero_rpi_end)
-    missing_rpi = check_xml_exists(missing_rpi_csv)
 
     for numero_rpi in missing_rpi_csv:
         print(f'PatentTransformer: Processando {numero_rpi}.xml...')
--- patent_app.py
+++ patent_app.py
@@ -7,7 +7,7 @@ import pandas as pd
 import asyncio
 
 from modules.rpi.patent_extractor import check_xml_exists, get_rpi_patentes, unzip, rename_xml
-from modules.rpi.patent_transformer import check_csv_exists, find_xml_in_missing_rpi_csv, extract_metadata, build_dataframe
+from modules.rpi.patent_transformer import check_csv_exists, parse_element
 
 url_template = 'https://revistas.inpi.gov.br/txt/P{}.zip'
 
@@ -17,30 +17,34 @@ if __name__ == '__main__':
     numero_rpi_start = int(input(f'PatentApp: Escreva o número de RPI inicial: '))
     numero_rpi_end = int(input(f'PatentApp: Escreva o número de RPI final: '))
 
-    missing_rpi = check_xml_exists(numero_rpi_start, numero_rpi_end)
+    missing_rpi_xml = check_xml_exists(numero_rpi_start, numero_rpi_end)
 
     missing_rpi_csv = check_csv_exists(numero_rpi_start, numero_rpi_end)
-    found_xml_files = find_xml_in_missing_rpi_csv(missing_rpi_csv)
 
-
-    if missing_rpi is None:
-        missing_rpi
+    if missing_rpi_xml is None:
+        missing_rpi_xml
     else:
-        get_rpi_patentes(url_template, missing_rpi)
-        unzip(missing_rpi)
-        rename_xml(missing_rpi)
+        get_rpi_patentes(url_template, missing_rpi_xml)
+        unzip(missing_rpi_xml)
+        rename_xml(missing_rpi_xml)
 
-    if found_xml_files is None:
-        find_xml_in_missing_rpi_csv
+    if missing_rpi_csv is None:
+        missing_rpi_csv
     else:
-        for numero_rpi in found_xml_files:
-            xml_file_name = 'P{}.xml'.format(numero_rpi)
-            tree = ET.parse(xml_file_name)
-            root = tree.getroot()
-            despacho = root.find('despacho')
+        for numero_rpi in missing_rpi_csv:
+        print(f'PatentTransformer: Processando {numero_rpi}.xml...')
+        tree = ET.parse(f'P{numero_rpi}.xml')
+        root = tree.getroot()
+        data = []
+        for child in root:
+            item = {}
+            parse_element(child, item)
+            data.append(item)
+            df = pd.DataFrame(data)
+            df.to_csv(f'P{numero_rpi}.csv')
+        print(f'PatentTransformer: Planilha {numero_rpi}.csv criada!')
 
-            extract_metadata(numero_rpi, despacho)
-            build_dataframe(found_xml_files)
+    
     
 
 
