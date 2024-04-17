1e06f4f0588ba9158967091cc48e7308c75e5f7c
Lucas F Aguiar <lucas.fernandes.df@gmail.com>
2024-04-17 17:32:48 +0000

    add: patent app documentarion

--- patent_app.py
+++ patent_app.py
@@ -17,9 +17,12 @@ if __name__ == '__main__':
     numero_rpi_start = int(input(f'PatentApp: Escreva o número de RPI inicial: '))
     numero_rpi_end = int(input(f'PatentApp: Escreva o número de RPI final: '))
 
+    # Initial Checklists
     missing_rpi_xml = check_xml_exists(numero_rpi_start, numero_rpi_end)
 
     missing_rpi_csv = check_csv_exists(numero_rpi_start, numero_rpi_end)
+    
+    # Extract data from source
 
     if missing_rpi_xml is None:
         missing_rpi_xml
@@ -28,23 +31,20 @@ if __name__ == '__main__':
         unzip(missing_rpi_xml)
         rename_xml(missing_rpi_xml)
 
+    # Transform into dataframes
+
     if missing_rpi_csv is None:
         missing_rpi_csv
     else:
         for numero_rpi in missing_rpi_csv:
-        print(f'PatentTransformer: Processando {numero_rpi}.xml...')
-        tree = ET.parse(f'P{numero_rpi}.xml')
-        root = tree.getroot()
-        data = []
-        for child in root:
-            item = {}
-            parse_element(child, item)
-            data.append(item)
-            df = pd.DataFrame(data)
-            df.to_csv(f'P{numero_rpi}.csv')
-        print(f'PatentTransformer: Planilha {numero_rpi}.csv criada!')
-
-    
-    
-
-
+            print(f'PatentTransformer: Processando {numero_rpi}.xml...')
+            tree = ET.parse(f'P{numero_rpi}.xml')
+            root = tree.getroot()
+            data = []
+            for child in root:
+                item = {}
+                parse_element(child, item)
+                data.append(item)
+                df = pd.DataFrame(data)
+                df.to_csv(f'P{numero_rpi}.csv')
+            print(f'PatentTransformer: Planilha {numero_rpi}.csv criada!')
